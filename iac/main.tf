3eed238d4ae8ce2caecdb8f315a465b8b7c71db1
William Amouh <williamamouh@Williams-Laptop.wind3.hub>
2024-04-29 03:16:40 +0200

    add cd

--- iac/main.tf
+++ iac/main.tf
@@ -16,7 +16,21 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0caef02b518350c8b"
   instance_type = "t2.micro"
 
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install -y docker
+              yum install docker-compose-plugin
+              systemctl enable docker
+              systemctl start docker
+              sudo chown $USER /var/run/docker.sock
+              docker run -p 80:80 -d nginx
+              EOF
+
   tags = {
     Name = "AppServerInstance"
   }
+}
+
+output "public_ip" {
+  value = aws_instance.app_server.public_ip
 }
