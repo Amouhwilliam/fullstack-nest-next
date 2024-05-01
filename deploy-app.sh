87cff8df23876f1eb9cf1700be25e6d8dbb6a2f3
William Amouh <williamamouh@Williams-Laptop.wind3.hub>
2024-05-01 08:35:42 +0200

    fix conflicts

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,9 +1,4 @@
 git pull
 cp .env.prod .env
-<<<<<<< HEAD
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d
-=======
-docker compose down
-docker compose -f docker-compose.prod.yml up -d
->>>>>>> 876fc1d (provide new server)
