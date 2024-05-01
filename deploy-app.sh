e111cc56d417a2a75f0074a9396071cf1f0608e5
__William_Amouh__ <amouh74@gmail.com>
2024-05-01 17:30:50 +0000

    update deployment script

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,5 +1,14 @@
 git pull
 git reset --hard origin/main
 cp .env.prod .env
+cd frontend
+sudo rm -rf node_modules
+yarn install
+yarn build
+cd ../api
+sudo rm -rf node_modules
+yarn install
+yarn build
+cd ..
 docker compose -f docker-compose.prod.yml down
 docker compose -f docker-compose.prod.yml up -d
